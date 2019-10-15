5af2d82357f8d33c395df4fbb6314e667cb73084
mohammad <mohammadsheikhian70@gmail.com>
2019-10-15 18:26:52 +0330

    Modified the threading requests

--- IO-bound-threading.py
+++ IO-bound-threading.py
@@ -7,11 +7,17 @@ import requests
 
 
 thread_local = threading.local()
-
-
 start_times = dict()
 end_times = dict()
-l = dict()
+duration_times = dict()
+request_threads = dict()
+list_of_duration_times = []
+min_duration_time = 0
+max_duration_time = 0
+mean_duration_time = 0
+count_of_requests = 1000
+count_of_threads = 10
+
 
 def get_session():
     if not hasattr(thread_local, "session"):
@@ -22,40 +28,31 @@ def get_session():
 def download_site(url):
     start_times[url[0]] = perf_counter()
     session = get_session()
-#    l[threading.current_thread().ident] = 'asd'
-    l[threading.get_ident()] = 'asd'
-    with session.get(url[1]) as response:
+    with session.request('VERIFY',url[1]) as response:
         end_times[url[0]] = perf_counter()
+        request_threads[url[0]] = (threading.get_ident(), response.status_code)
 
 
 def download_all_sites(sites):
-    with concurrent.futures.ThreadPoolExecutor(max_workers=2) as executor:
+    with concurrent.futures.ThreadPoolExecutor(max_workers=count_of_threads) as executor:
         executor.map(download_site, sites)
 
 
 if __name__ == "__main__":
     sites = []
-    for i in range(50):
-        sites.append((i, 'http://0.0.0.0:8083/apiv1/version'))
+    for i in range(count_of_requests):
+        sites.append((i, 'http://192.168.1.80/apiv1/tokens/86/codes/924F0A6214F59712?primitive=yes'))
 
-#    for i in range(2):
-#        sites.append((i * 2, 'https://alpha-maestro.carrene.com/apiv1/version'))
-#
-#
-#    sites = [
-#        "https://www.jython.org",
-#        "http://olympus.realpython.org/dice",
-#    ] * 40
     start_time = time.time()
     download_all_sites(sites)
     duration = time.time() - start_time
     print(f"Downloaded {len(sites)} in {duration} seconds")
-#    print('Start\n')
-#    print(start_times)
-#    print('\nEndi\n')
-    print(end_times)
 
-    for i in range(50):
-        print(end_times[i]-start_times[i])
+    for i in range(count_of_requests):
+        duration_times[i] = end_times[i]-start_times[i]
 
-    print(l)
+    list_of_duration_times = [v for v in duration_times.values()]
+    list_of_duration_times.sort()
+    min_duration_time = list_of_duration_times[0]
+    max_duration_time = list_of_duration_times[-1]
+    #print(request_threads)
